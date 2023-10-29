29b693326301be7a375e102e288a09ea9a8abeef
Bruno <pazosbruno@gmail.com>
2023-10-29 17:10:31 -0300

    fix typo device<->device_group

--- src/device_group/router.py
+++ src/device_group/router.py
@@ -7,19 +7,19 @@ router = APIRouter()
 
 
 @router.post("/device_groups/", response_model=schemas.DeviceGroup)
-def create_device(device_group: schemas.DeviceGroupCreate, db: Session = Depends(get_db)):
-    db_device = service.get_device_group_by_name(db, device_group_name=device_group.name)
-    if db_device:
+def create_device_group(device_group: schemas.DeviceGroupCreate, db: Session = Depends(get_db)):
+    db_device_group = service.get_device_group_by_name(db, device_group_name=device_group.name)
+    if db_device_group:
         raise HTTPException(status_code=400, detail="Device group name already registered")
-    return db_device
+    return db_device_group
 
 
 @router.get("/device_groups/{device_id}", response_model=schemas.DeviceGroup)
 def read_device_group(device_group_id: int, db: Session = Depends(get_db)):
-    db_device = service.get_device_group(db, device_group_id=device_group_id)
-    if db_device is None:
+    db_device_group = service.get_device_group(db, device_group_id=device_group_id)
+    if db_device_group is None:
         raise HTTPException(status_code=404, detail="Device group not found")
-    return db_device
+    return db_device_group
 
 @router.get("/device_groups/", response_model=list[schemas.DeviceGroup])
 def read_device_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -29,8 +29,8 @@ def read_device_groups(skip: int = 0, limit: int = 100, db: Session = Depends(ge
 def update_device_group(
     device_group_id: int, device_group: schemas.DeviceGroupUpdate, db: Session = Depends(get_db)
 ):
-    db_device = read_device_group(device_group_id, db)
-    updated_device = service.update_device_group(db, db_device, updated_device_group=device_group)
+    db_device_group = read_device_group(device_group_id, db)
+    updated_device = service.update_device_group(db, db_device_group, updated_device_group=device_group)
     if not updated_device:
         raise HTTPException(status_code=400, detail="Device group could not be updated")
     return updated_device
@@ -38,8 +38,8 @@ def update_device_group(
 
 @router.delete("/device_groups/{device_id}", response_model=schemas.DeviceGroupDelete)
 def delete_device(device_group_id: int, db: Session = Depends(get_db)):
-    db_device_group = read_device_group(device_group_id, db)
-    deleted_device_group_id = service.delete_device_group(db, db_device_group)
+    db_device_group_group = read_device_group(device_group_id, db)
+    deleted_device_group_id = service.delete_device_group(db, db_device_group_group)
     if not deleted_device_group_id:
         raise HTTPException(status_code=400, detail="UserGroup could not be deleted")
     return {
