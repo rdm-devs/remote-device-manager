422f6ac028435166166e22c12e0e76a86a0c9540
Bruno <pazosbruno@gmail.com>
2023-10-29 17:17:14 -0300

    adding tenant router to main app

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from .user.router import router as user_router
 from .user_group.router import router as user_group_router
 from .device.router import router as device_router
 from .device_group.router import router as device_group_router
+from .tenant.router import router as tenant_router
 from .database import engine, Base
 
 load_dotenv()
@@ -20,3 +21,4 @@ app.include_router(user_router)
 app.include_router(user_group_router)
 app.include_router(device_router)
 app.include_router(device_group_router)
+app.include_router(tenant_router)
