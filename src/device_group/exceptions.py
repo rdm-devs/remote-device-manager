dc3be9aa293e420bef56e8bf3c91b9177e0bebed
Bruno <pazosbruno@gmail.com>
2023-11-12 18:54:16 -0300

    added custom constants and exceptions to the device group module

--- src/device_group/exceptions.py
+++ src/device_group/exceptions.py
@@ -4,3 +4,11 @@ from src.exceptions import NotFound, BadRequest
 
 class DeviceGroupNotFoundError(NotFound):
     DETAIL = ErrorCode.DEVICE_GROUP_NOT_FOUND
+
+
+class DeviceGroupNameTakenError(BadRequest):
+    DETAIL = ErrorCode.DEVICE_GROUP_NAME_TAKEN
+
+
+class InvalidDeviceGroupAttrsError(BadRequest):
+    DETAIL = ErrorCode.DEVICE_GROUP_INVALID_ATTRS
