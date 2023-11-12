9d67834701e4ed6e02914223460d2e1402ce4d9d
Bruno <pazosbruno@gmail.com>
2023-11-12 18:55:29 -0300

    simplified code (using exceptions in service) and fixed some typos

--- src/device_group/router.py
+++ src/device_group/router.py
@@ -8,24 +8,20 @@ router = APIRouter()
 
 @router.post("/device_groups/", response_model=schemas.DeviceGroup)
 def create_device_group(device_group: schemas.DeviceGroupCreate, db: Session = Depends(get_db)):
-    db_device_group = service.get_device_group_by_name(db, device_group_name=device_group.name)
-    if db_device_group:
-        raise HTTPException(status_code=400, detail="Device group name already registered")
+    db_device_group = service.create_device_group(db, device_group)
     return db_device_group
 
 
-@router.get("/device_groups/{device_id}", response_model=schemas.DeviceGroup)
+@router.get("/device_groups/{device_group_id}", response_model=schemas.DeviceGroup)
 def read_device_group(device_group_id: int, db: Session = Depends(get_db)):
-    db_device_group = service.get_device_group(db, device_group_id=device_group_id)
-    if db_device_group is None:
-        raise HTTPException(status_code=404, detail="Device group not found")
+    db_device_group = service.get_device_group(db, device_group_id)
     return db_device_group
 
 @router.get("/device_groups/", response_model=list[schemas.DeviceGroup])
 def read_device_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return service.get_device_groups(db, skip=skip, limit=limit)
 
-@router.patch("/device_groups/{device_id}", response_model=schemas.DeviceGroup)
+@router.patch("/device_groups/{device_group_id}", response_model=schemas.DeviceGroup)
 def update_device_group(
     device_group_id: int, device_group: schemas.DeviceGroupUpdate, db: Session = Depends(get_db)
 ):
@@ -36,7 +32,7 @@ def update_device_group(
     return updated_device
 
 
-@router.delete("/device_groups/{device_id}", response_model=schemas.DeviceGroupDelete)
+@router.delete("/device_groups/{device_group_id}", response_model=schemas.DeviceGroupDelete)
 def delete_device(device_group_id: int, db: Session = Depends(get_db)):
     db_device_group_group = read_device_group(device_group_id, db)
     deleted_device_group_id = service.delete_device_group(db, db_device_group_group)
