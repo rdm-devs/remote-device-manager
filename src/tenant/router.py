17878690a0bac86b16ebc346866ec34d26b5c693
Bruno <pazosbruno@gmail.com>
2023-11-15 19:10:36 -0300

    simplifying route code, services raise exceptions instead of routes

--- src/tenant/router.py
+++ src/tenant/router.py
@@ -8,17 +8,13 @@ router = APIRouter()
 
 @router.post("/tenants/", response_model=schemas.Tenant)
 def create_tenant(tenant: schemas.TenantCreate, db: Session = Depends(get_db)):
-    db_tenant = service.get_tenant_by_name(db, tenant_name=tenant.name)
-    if db_tenant:
-        raise HTTPException(status_code=400, detail="Tenant name already registered")
+    db_tenant = service.create_tenant(db, tenant)
     return db_tenant
 
 
 @router.get("/tenants/{tenant_id}", response_model=schemas.Tenant)
 def read_tenant(tenant_id: int, db: Session = Depends(get_db)):
     db_tenant = service.get_tenant(db, tenant_id=tenant_id)
-    if db_tenant is None:
-        raise HTTPException(status_code=404, detail="Tenant not found")
     return db_tenant
 
 
@@ -33,8 +29,6 @@ def update_tenant(
 ):
     db_tenant = read_tenant(tenant_id, db)
     updated_tenant = service.update_tenant(db, db_tenant, updated_tenant=tenant)
-    if not updated_tenant:
-        raise HTTPException(status_code=400, detail="Tenant could not be updated")
     return updated_tenant
 
 
@@ -42,8 +36,6 @@ def update_tenant(
 def delete_tenant(tenant_id: int, db: Session = Depends(get_db)):
     db_tenant = read_tenant(tenant_id, db)
     deleted_tenant_id = service.delete_tenant(db, db_tenant)
-    if not deleted_tenant_id:
-        raise HTTPException(status_code=400, detail="Tenant could not be deleted")
     return {
         "id": deleted_tenant_id,
         "msg": f"tenant group {deleted_tenant_id} removed succesfully!",
