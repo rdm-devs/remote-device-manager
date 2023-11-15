d371c1892cebb162cacd5c6b47dbb9add123ea64
Bruno <pazosbruno@gmail.com>
2023-11-15 19:12:59 -0300

    adding some sanity checks and raising exceptions when those checks fail

--- src/tenant/service.py
+++ src/tenant/service.py
@@ -1,27 +1,22 @@
 from sqlalchemy.orm import Session
 from . import schemas, models
-from src.tenant.exceptions import TenantNotFoundError
+from src.tenant.exceptions import TenantNameTakenError, TenantNotFoundError
 
 
 def check_tenant_exists(db: Session, tenant_id: int):
-    tenant = (
-        db.query(models.Tenant)
-        .filter(models.Tenant.id == tenant_id)
-        .first()
-    )
+    tenant = db.query(models.Tenant).filter(models.Tenant.id == tenant_id).first()
     if not tenant:
         raise TenantNotFoundError()
 
 
-def create_tenant(db: Session, tenant: schemas.TenantCreate):
-    db_tenant = models.Tenant(**tenant)
-    db.add(db_tenant)
-    db.commit()
-    db.refresh(db_tenant)
-    return db_tenant
+def check_tenant_name_taken(db: Session, tenant_name: str):
+    tenant = db.query(models.Tenant).filter(models.Tenant.name == tenant_name).first()
+    if tenant:
+        raise TenantNameTakenError()
 
 
 def get_tenant(db: Session, tenant_id: int):
+    check_tenant_exists(db, tenant_id)
     return db.query(models.Tenant).filter(models.Tenant.id == tenant_id).first()
 
 
@@ -30,7 +25,22 @@ def get_tenants(db: Session, skip: int = 0, limit: int = 100):
 
 
 def get_tenant_by_name(db: Session, tenant_name: str):
-    return db.query(models.Tenant).filter(models.Tenant.name == tenant_name).first()
+    db_tenant = (
+        db.query(models.Tenant).filter(models.Tenant.name == tenant_name).first()
+    )
+    if not db_tenant:
+        raise TenantNotFoundError()
+    return db_tenant
+
+
+def create_tenant(db: Session, tenant: schemas.TenantCreate):
+    check_tenant_name_taken(db, tenant.name)
+
+    db_tenant = models.Tenant(**tenant.model_dump())
+    db.add(db_tenant)
+    db.commit()
+    db.refresh(db_tenant)
+    return db_tenant
 
 
 def update_tenant(
@@ -38,6 +48,10 @@ def update_tenant(
     db_tenant: schemas.Tenant,
     updated_tenant: schemas.TenantUpdate,
 ):
+    # sanity checks
+    check_tenant_exists(db, db_tenant.id)
+    check_tenant_name_taken(db, updated_tenant.name)
+
     db.query(models.Tenant).filter(models.Tenant.id == db_tenant.id).update(
         values=updated_tenant.model_dump()
     )
@@ -48,6 +62,9 @@ def update_tenant(
 
 
 def delete_tenant(db: Session, db_tenant: schemas.Tenant):
+    # sanity check
+    check_tenant_exists(db, tenant_id=db_tenant.id)
+
     db.delete(db_tenant)
     db.commit()
     return db_tenant.id
