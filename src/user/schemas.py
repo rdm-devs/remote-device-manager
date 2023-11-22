fe5a42485aa2e418616f1a51532567214c98dedf
Bruno <pazosbruno@gmail.com>
2023-11-21 21:23:33 -0300

    restricting schemas to forbid extra arguments on creation and updates + when updating a user, the password is optional

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -1,26 +1,27 @@
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
-from typing import Optional
 
 
 class UserBase(BaseModel):  # used as common fields
     email: EmailStr
-    group_id: int
-    last_login: datetime
+    last_login: datetime = datetime.now()
 
 
 class UserCreate(UserBase):  # used when creating user
     password: str
 
+    model_config = {"extra": "forbid"}
+
 
 class User(UserBase):  # used when reading user info
     id: int
-
     model_config = {"from_attributes": True}
 
 
-class UserUpdate(UserCreate):
-    pass
+class UserUpdate(UserBase):
+    password: str | None = None
+
+    model_config = {"extra": "forbid"}
 
 
 class UserDelete(BaseModel):
