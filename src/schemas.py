5b2a00e6001e96917dd3b10069ad42a1f1fad85b
Bruno <pazosbruno@gmail.com>
2023-11-21 21:13:45 -0300

    created common User/UserGroup schemas to avoid circular imports

---
+++ src/schemas.py
@@ -0,0 +1,10 @@
+from .user_group.schemas import UserGroup
+from .user.schemas import User
+
+# To solve many to many relationship and circular imports we create two new schemas.
+class UserGroupWithUsers(UserGroup):
+    users: list[User]
+
+
+class UserWithUserGroups(User):
+    user_groups: list[UserGroup]
