18feca27eeeb49b4f2f26f21f8628c471d3750f0
Bruno <pazosbruno@gmail.com>
2023-11-21 21:14:23 -0300

    added useful constants and exceptions for User

---
+++ src/user/exceptions.py
@@ -0,0 +1,13 @@
+from src.user.constants import ErrorCode
+from src.exceptions import NotFound, BadRequest
+
+
+class UserNotFoundError(NotFound):
+    DETAIL = ErrorCode.USER_NOT_FOUND
+
+
+class UserEmailTakenError(BadRequest):
+    DETAIL = ErrorCode.USER_EMAIL_TAKEN
+
+class UserInvalidPasswordError(BadRequest):
+    DETAIL = ErrorCode.USER_INVALID_PASSWORD
