616648e778180db941654c8dbb3357765cd3c738
Bruno <pazosbruno@gmail.com>
2023-11-28 23:05:01 -0300

    defining 'module level' schemas to avoid circular imports

--- src/schemas.py
+++ src/schemas.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from .user_group.schemas import UserGroup
 from .user.schemas import User
 
 # To solve many to many relationship and circular imports we create two new schemas.
 class UserGroupWithUsers(UserGroup):
-    users: list[User]
+    users: list[User] = []
 
 
 class UserWithUserGroups(User):
-    user_groups: list[UserGroup]
+    user_groups: list[UserGroup] = []
