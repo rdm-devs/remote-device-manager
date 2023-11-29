542890415a54327499a76302ff493fbc79c6e3d1
Bruno <pazosbruno@gmail.com>
2023-11-29 15:20:31 -0300

    added new route to get devices from a device group

--- src/device_group/router.py
+++ src/device_group/router.py
@@ -2,12 +2,15 @@ from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from . import service, schemas
+from ..device.schemas import Device
 
 router = APIRouter()
 
 
 @router.post("/device_groups/", response_model=schemas.DeviceGroup)
-def create_device_group(device_group: schemas.DeviceGroupCreate, db: Session = Depends(get_db)):
+def create_device_group(
+    device_group: schemas.DeviceGroupCreate, db: Session = Depends(get_db)
+):
     db_device_group = service.create_device_group(db, device_group)
     return db_device_group
 
@@ -17,28 +20,40 @@ def read_device_group(device_group_id: int, db: Session = Depends(get_db)):
     db_device_group = service.get_device_group(db, device_group_id)
     return db_device_group
 
+
 @router.get("/device_groups/", response_model=list[schemas.DeviceGroup])
 def read_device_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return service.get_device_groups(db, skip=skip, limit=limit)
 
+
 @router.patch("/device_groups/{device_group_id}", response_model=schemas.DeviceGroup)
 def update_device_group(
-    device_group_id: int, device_group: schemas.DeviceGroupUpdate, db: Session = Depends(get_db)
+    device_group_id: int,
+    device_group: schemas.DeviceGroupUpdate,
+    db: Session = Depends(get_db),
 ):
-    db_device_group = read_device_group(device_group_id, db)
-    updated_device = service.update_device_group(db, db_device_group, updated_device_group=device_group)
-    if not updated_device:
-        raise HTTPException(status_code=400, detail="Device group could not be updated")
+    updated_device = service.update_device_group(
+        db, device_group_id, updated_device_group=device_group
+    )
     return updated_device
 
 
-@router.delete("/device_groups/{device_group_id}", response_model=schemas.DeviceGroupDelete)
-def delete_device(device_group_id: int, db: Session = Depends(get_db)):
+@router.delete(
+    "/device_groups/{device_group_id}", response_model=schemas.DeviceGroupDelete
+)
+def delete_device_group(device_group_id: int, db: Session = Depends(get_db)):
     db_device_group_group = read_device_group(device_group_id, db)
     deleted_device_group_id = service.delete_device_group(db, db_device_group_group)
-    if not deleted_device_group_id:
-        raise HTTPException(status_code=400, detail="UserGroup could not be deleted")
     return {
         "id": deleted_device_group_id,
         "msg": f"Device group {deleted_device_group_id} removed succesfully!",
     }
+
+
+@router.get("/device_groups/{device_group_id}/devices", response_model=list[Device])
+def get_devices_from_device_group(
+    device_group_id: int,
+    db: Session = Depends(get_db),
+):
+    db_devices = service.get_devices_from_device_group(db, device_group_id)
+    return db_devices
