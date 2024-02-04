2e37d6f3bd559928e2c9ac1ab20fa7b7363a717c
Bruno <pazosbruno@gmail.com>
2024-02-04 18:53:46 -0300

    adding routes prefix + minor edits

--- src/tenant/router.py
+++ src/tenant/router.py
@@ -3,27 +3,27 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from . import service, schemas
 
-router = APIRouter()
+router = APIRouter(prefix="/tenants", tags=["tenants"])
 
 
-@router.post("/tenants/", response_model=schemas.Tenant)
+@router.post("/", response_model=schemas.Tenant)
 def create_tenant(tenant: schemas.TenantCreate, db: Session = Depends(get_db)):
     db_tenant = service.create_tenant(db, tenant)
     return db_tenant
 
 
-@router.get("/tenants/{tenant_id}", response_model=schemas.Tenant)
+@router.get("/{tenant_id}", response_model=schemas.Tenant)
 def read_tenant(tenant_id: int, db: Session = Depends(get_db)):
     db_tenant = service.get_tenant(db, tenant_id=tenant_id)
     return db_tenant
 
 
-@router.get("/tenants/", response_model=list[schemas.Tenant])
+@router.get("/", response_model=list[schemas.Tenant])
 def read_tenants(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return service.get_tenants(db, skip=skip, limit=limit)
 
 
-@router.patch("/tenants/{tenant_id}", response_model=schemas.Tenant)
+@router.patch("/{tenant_id}", response_model=schemas.Tenant)
 def update_tenant(
     tenant_id: int, tenant: schemas.TenantUpdate, db: Session = Depends(get_db)
 ):
@@ -32,11 +32,11 @@ def update_tenant(
     return updated_tenant
 
 
-@router.delete("/tenants/{tenant_id}", response_model=schemas.TenantDelete)
+@router.delete("/{tenant_id}", response_model=schemas.TenantDelete)
 def delete_tenant(tenant_id: int, db: Session = Depends(get_db)):
     db_tenant = read_tenant(tenant_id, db)
     deleted_tenant_id = service.delete_tenant(db, db_tenant)
     return {
         "id": deleted_tenant_id,
-        "msg": f"tenant group {deleted_tenant_id} removed succesfully!",
+        "msg": f"Tenant {deleted_tenant_id} removed succesfully!",
     }
