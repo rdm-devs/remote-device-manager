a337f47f9b3a0b0a61513c60a3333d44eb686079
Bruno <pazosbruno@gmail.com>
2024-02-10 18:33:04 -0300

    removed Log in favor of Trigger based db logging

--- src/log/constants.py
+++
@@ -1,2 +0,0 @@
-class ErrorCode:
-    LOG_ENTRY_NOT_FOUND = "Log entry not found."
--- src/log/exceptions.py
+++
@@ -1,6 +0,0 @@
-from src.log.constants import ErrorCode
-from src.exceptions import NotFound, BadRequest
-
-
-class LogEntryNotFoundError(NotFound):
-    DETAIL = ErrorCode.LOG_ENTRY_NOT_FOUND
