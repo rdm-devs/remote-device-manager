16a74fc8414fe8919e591ce11985c54cccaf7c0f
Bruno <pazosbruno@gmail.com>
2024-03-24 23:20:23 -0300

    renaming exceptions

--- src/tag/exceptions.py
+++ src/tag/exceptions.py
@@ -1,6 +1,9 @@
 from src.tag.constants import ErrorCode
-from src.exceptions import NotFound
+from src.exceptions import NotFound, BadRequest
 
 
-class TagNotFoundError(NotFound):
+class TagNotFound(NotFound):
     DETAIL = ErrorCode.TAG_NOT_FOUND
+
+class TagNameTaken(BadRequest):
+    DETAIL = ErrorCode.TAG_NAME_TAKEN
