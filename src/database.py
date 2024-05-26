ec5651f7fa5c61f2988a13d2a17032773e7ca02e
Bruno <pazosbruno@gmail.com>
2024-05-26 18:17:49 -0300

    added a custom __repr__() method to the Base model in order to make the debugging process easier.

--- src/database.py
+++ src/database.py
@@ -13,7 +13,23 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+def keyvalgen(obj):
+    """Generate attr name/val pairs, filtering out SQLA attrs."""
+    excl = ("_sa_adapter", "_sa_instance_state")
+    for k, v in vars(obj).items():
+        if not k.startswith("_") and not any(hasattr(v, a) for a in excl):
+            yield k, v
+
+
+class Base:
+
+    def __repr__(self):
+        params = ", ".join(f"{k}={v}" for k, v in keyvalgen(self))
+        return f"{self.__class__.__name__}({params})"
+
+
+Base = declarative_base(cls=Base)
 
 
 # Dependency
