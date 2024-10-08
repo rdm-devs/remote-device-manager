4aeeebc251427639c720352fe172ba8cdae81e3e
Bruno <pazosbruno@gmail.com>
2024-10-08 11:16:21 -0300

    - added new service functions for Device for share URL creation, verification and expiration and to allow users to connect to a Device through a share URL. - updated get_devices service function to automatically expire invalid share URLs before retrieving a list of Devices. - marked a service function for future deletion.

--- src/device/service.py
+++ src/device/service.py
@@ -1,7 +1,11 @@
-from datetime import datetime
+import os
+import time
+from datetime import datetime, timedelta, UTC
+from jose import JWTError, jwt
 from typing import Optional
 from sqlalchemy import select, update
 from sqlalchemy.orm import Session
+from src.auth.utils import create_otp, create_connection_url
 from src.device import schemas, models, exceptions
 from src.entity.service import create_entity_auto, update_entity_tags
 from src.folder.models import Folder
@@ -29,6 +33,7 @@ def get_device(db: Session, device_id: int) -> models.Device:
     return device
 
 
+# TODO: marking this function for deletion. check is not used anywhere first.
 def get_devices_from_tenant(db: Session, tenant_id: int):
     return (
         db.query(models.Device)
@@ -37,17 +42,23 @@ def get_devices_from_tenant(db: Session, tenant_id: int):
     )
 
 
+def expire_invalid_share_urls(db: Session) -> None:
+    db.execute(
+        update(models.Device)
+        .where(models.Device.share_url.is_not(None))
+        .where(models.Device.share_url_expires_at < datetime.now(UTC))
+        .values(share_url=None, share_url_expires_at=None)
+    )
+
+
 def get_devices(db: Session, user_id: int):
+    expire_invalid_share_urls(db)
     user = get_user(db, user_id)
     if user.is_admin:
         devices = select(models.Device)
     else:
-        # folders = get_folders(db, user.id).subquery()
         device_ids = user.get_device_ids()
-        devices = select(models.Device).where(
-            # models.Device.folder_id.in_(folders)
-            models.Device.id.in_(device_ids)
-        )
+        devices = select(models.Device).where(models.Device.id.in_(device_ids))
     return devices
 
 
@@ -74,7 +85,7 @@ def create_device(db: Session, device: schemas.DeviceCreate):
 
 def update_device(
     db: Session, db_device: schemas.Device, updated_device: schemas.DeviceUpdate
-):
+) -> schemas.Device:
     # sanity checks
     values = updated_device.model_dump(exclude_unset=True)
     device = get_device(db, db_device.id)
@@ -100,9 +111,7 @@ def update_device(
     return device
 
 
-def update_device_heartbeat(
-    db: Session, device_id: int, heartbeat: schemas.HeartBeat
-):
+def update_device_heartbeat(db: Session, device_id: int, heartbeat: schemas.HeartBeat):
     # sanity checks
     values = heartbeat.model_dump(exclude_none=True)
     timestamp = datetime.now()
@@ -121,3 +130,70 @@ def delete_device(db: Session, db_device: schemas.Device):
     db.delete(db_device)
     db.commit()
     return db_device.id
+
+
+def create_share_url(
+    user_id: int, device_id: int, expiration_hours: int
+) -> tuple[schemas.ShareDeviceURL, datetime]:
+    expiration_dt = datetime.now(UTC) + timedelta(hours=expiration_hours)
+    to_encode = {
+        "user_id": user_id,
+        "device_id": device_id,
+        "timestamp": time.time(),
+        "exp": expiration_dt,
+    }
+    share_hash = jwt.encode(
+        to_encode, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM")
+    )
+    share_url = f"{os.getenv(f"API_SITE_DOMAIN_{os.getenv("ENV")}")}/devices/shared?id={share_hash}"
+    return schemas.ShareDeviceURL(url=share_url), expiration_dt
+
+
+def share_device(
+    db: Session, user_id: int, device_id: int, share_params: schemas.ShareParams
+) -> schemas.ShareDeviceURL:
+    # extracting expiration_hours from share_params
+    expiration_hours = share_params.expiration_hours
+
+    # creating share_url with the corresponding response schema
+    share_url, expiration_dt = create_share_url(user_id, device_id, expiration_hours)
+
+    # updating device attributes (share_url + share exp time)
+    device = get_device(db, device_id)
+    device = update_device(
+        db,
+        device,
+        schemas.DeviceUpdate(
+            share_url=share_url.url,
+            share_url_expires_at=expiration_dt,
+            tags=device.tags,
+        ),
+    )
+
+    return share_url
+
+
+def verify_share_url(db: Session, token: str) -> str:
+    # decoding data to obtain shared device metadata
+    shared_device_metadata = jwt.decode(
+        token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")]
+    )
+
+    # checking that the device share_url is not null
+    device = db.scalars(
+        select(models.Device)
+        .where(models.Device.id == shared_device_metadata["device_id"])
+        .where(models.Device.share_url.is_not(None))
+    ).first()
+
+    if not device:
+        raise exceptions.DeviceNotFound()
+
+    has_expired = datetime.now() > device.share_url_expires_at
+    if has_expired:
+        raise exceptions.ExpiredShareDeviceURL()
+
+    # URL is valid, redirecting the user
+    otp = create_otp()
+    redirect_url = create_connection_url(db, device.id, otp)
+    return redirect_url
