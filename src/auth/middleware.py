f3fc9dcb81e3d355c3f0804230c68e5cf88fdbf3
Bruno <pazosbruno@gmail.com>
2024-10-17 10:08:58 -0300

    preventing error 500 by returning a JSON response with the appropiated status code and message when the jwt is invalid

--- src/auth/middleware.py
+++ src/auth/middleware.py
@@ -1,7 +1,9 @@
 from contextvars import ContextVar
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
+from fastapi.responses import JSONResponse
 from src.auth.dependencies import get_current_user
+from src.auth.exceptions import InvalidCredentials
 from src.database import get_db
 from src.audit_mixin import _auth_user_ctx
 
@@ -12,8 +14,12 @@ class AuthUserRequestContextMiddleware(BaseHTTPMiddleware):
             "authorization" in request.headers.keys()
             and "Bearer" in request.headers["authorization"]
         ):
-            token = request.headers["authorization"].replace("Bearer ", "")
-            user = await get_current_user(token, next(get_db()))
-            _auth_user_ctx.set(user.id)
+            try:
+                token = request.headers["authorization"].replace("Bearer ", "")
+                user = await get_current_user(token, next(get_db()))
+                _auth_user_ctx.set(user.id)
+            except InvalidCredentials as exc:
+                return JSONResponse(content=exc.detail, status_code=exc.status_code)
+
         response = await call_next(request)
         return response
